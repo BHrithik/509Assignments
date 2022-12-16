88e52d738cc2d97468e443a5aae5c0c7d9ae170a
hrithik <hrithik.b@beehyv.com>
2022-12-16 02:35:21 -0800

    nen edustha

---
+++ TicTacToe/server.py
@@ -0,0 +1,129 @@
+from pyexpat.errors import messages
+from flask import Flask, render_template, request, url_for, flash, redirect
+from logic import TicTacToe
+from easy import Easy
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'your secret key'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd96eeaee39a7aaf82d02dc9e3dc10407b2d9b97883b5c8b5'
+
+
+easyMove = Easy() # Difficulty is left as improvement
+game = TicTacToe(easyMove)
+
+
+@app.route('/', methods=('POST','GET')) # Link to home page
+def index():
+    return render_template('index.html')
+
+@app.route('/multiplayer', methods=('POST','GET'))
+def multiplayer():
+    if request.method == 'POST':
+        name1 = request.form['name1']
+        S1 = request.form['options1']
+        name2 = request.form['name2']
+        if(S1 == "X"):
+            S2 = "O"
+        else :
+            S2 = "X"
+        if not name1:
+            flash('Name1 is required!')
+        elif not name2:
+            flash('Name2 is required for multy player')
+        else:
+            print("Player 1 is ",name1)
+            print("Symbol 1 is ", S1)
+            print("Player 2 is ",name2)
+            print("Symbol 2 is ", S2)
+            game.name1 = name1
+            game.name2 = name2
+            game.s1 = S1
+            game.s2 = S2
+            return redirect(url_for('play'))
+    return render_template('multi.html')
+
+@app.route('/singleplayer', methods=('POST','GET'))
+def singleplayer():
+    if request.method == 'POST':
+        name1 = request.form['name11']
+        S1 = request.form['options11']
+        if not name1:
+            flash('Name of the player is required!')
+        else:
+            game.isSinglePlayer = True
+            game.name2 = "bot"
+            game.name1 = name1
+            if(S1 == "X"):
+                S2 = "O"
+            else :
+                S2 = "X"
+            game.s1 = S1
+            game.s2 = S2
+            name2 = game.name2
+            print("Player 1 is ",name1)
+            print("Symbol 1 is ", S1)
+            print("Player 2 is ",name2)
+            print("Symbol 2 is ", S2)
+            return redirect(url_for('play'))
+        print("Player 1 is ",name1)
+    return render_template('single.html')
+
+@app.get('/aboutme')
+def aboutme():
+    return "I am student at UW"
+
+@app.get('/play')
+def play():
+    game.print_board()
+    if(game.isSinglePlayer == True and game.name1[0] == "DefaultName1"):
+        print("deen thalli")
+        return redirect(url_for('index'))
+    if(game.isSinglePlayer == False and game.name1[0] == "DefaultName1" and game.name2[0] == "bot"):
+        return redirect(url_for('index'))
+    message = ""
+    if(game.get_winner == None):
+        message = ""
+    elif(game.get_winner == game.s1):
+        message = "Congratulations " + game.name1 + " you have won the game!"
+    elif(game.get_winner == game.s2):
+        message = "Congratulations " + game.name2 + " you have won the game!"
+    data = {
+        "board": game.board,
+        "len1": range(len(game.board)),
+        "len2": range(len(game.board[0])),
+        "message": message,
+    }
+    return render_template('play.html',data=data)
+
+@app.get('/playing/<int:pos1>/<int:pos2>')
+def playing(pos1,pos2):
+    if(game.board[pos1][pos2] == " "):
+        if(game.count%2 == 0):
+            game.input(game.s1[0],pos1,pos2)
+            print("Move no.",game.count)
+            print("symbol",game.s1,"in",pos1,pos2)
+            if(game.isSinglePlayer):
+                game.computerMove(game.s2[0])
+        else:
+            game.input(game.s2[0],pos1,pos2)
+            print("Player 2 move")
+            print("symbol",game.s2,"in",pos1,pos2)
+            game.count = game.count+1
+    return redirect(url_for('play'))
+    
+
+
+def boardToDict(board):
+    boardDict = {
+                "00": board[0][0],
+                "01": board[0][1],
+                "02": board[0][2],
+                "10": board[1][0],
+                "11": board[1][1],
+                "12": board[1][2],
+                "20": board[2][0],
+                "21": board[2][1],
+                "22": board[2][2]
+                }
+    return boardDict
