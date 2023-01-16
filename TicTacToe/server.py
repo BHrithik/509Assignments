5b7ec90bc285bcec82076f375687fc7f06ea3a6e
hrithik <hrithik.b@beehyv.com>
2023-01-16 12:55:18 -0800

    update for deploying

--- TicTacToe/server.py
+++ TicTacToe/server.py
@@ -166,4 +166,7 @@ def stats():
         "graph2": graphJSON2
     }
     return render_template("statistics.html", graphs = graphs)
-    
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
